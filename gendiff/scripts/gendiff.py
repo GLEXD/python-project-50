f2cde0345ab19a23b452d36b0db2e340a1a634f3
GLEXD <glebushekbur@gmail.com>
2025-05-04 22:31:32 +0400

    3rd step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,10 @@
 import argparse
+import json
+from pathlib import Path
+
+
+def parse_file(file_path):
+    return json.load(open(file_path))
 
 
 def main():
@@ -12,6 +18,14 @@ def main():
     parser.add_argument("second_file")
     parser.add_argument('-f' ,'--format', help='set format of output')
     args = parser.parse_args()
+
+
+    data1 = parse_file(args.first_file)
+    data2 = parse_file(args.second_file)
+
+    print("File 1:", data1)
+    print("File 2:", data2)
+
 
 if __name__ == "__main__":
     main()
