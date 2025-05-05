4da284ccbee07d02b83f28d83fb16c81001978ca
GLEXD <glebushekbur@gmail.com>
2025-05-05 21:12:17 +0400

    Fixing another issues

--- tests/test_compare_json.py
+++ tests/test_compare_json.py
@@ -1,11 +1,13 @@
 import json
-import pytest
-
 
 from gendiff.compare_json import compare_jsons
+
+
 def load_json(file_path):
     with open(file_path) as f:
         return json.load(f)
+
+
 def test_json_comparison():
     file1 = "tests/test_data/file1.json"
     file2 = "tests/test_data/file2.json"
