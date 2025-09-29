0ec6d11ef2938596d039904e957035b72fd568a2
GLEXD <glebushekbur@gmail.com>
2025-09-29 21:11:36 +0400

    Some details

---
+++ gendiff/gendiff.py
@@ -0,0 +1,10 @@
+from gendiff.parser import get_data
+from gendiff.diff import build_diff
+from gendiff.formater import format_diff
+
+
+def generate_diff(file_1, file_2, format="stylish"):
+    dict_1 = get_data(file_1)
+    dict_2 = get_data(file_2)
+    diff = build_diff(dict_1, dict_2)
+    return format_diff(diff, format)
---
+++ gendiff/parser.py
@@ -0,0 +1,27 @@
+import json
+import yaml
+import os
+
+
+class ExtensionError(Exception):
+    pass
+
+
+def parse_data(file, extension):
+    if extension == 'json':
+        return json.loads(file)
+    elif extension in ['yml', 'yaml']:
+        return yaml.load(file, Loader=yaml.Loader)
+    else:
+        raise ExtensionError(f'Unsupported file format: {extension}')
+
+
+def get_data(file_path):
+    extension = os.path.splitext(file_path)[1][1:].lower()
+    file_cont = file_read(file_path)
+    return parse_data(file_cont, extension)
+
+
+def file_read(file_path):
+    with open(file_path) as file:
+        return file.read()
